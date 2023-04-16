19d560c645123c60b83d5605adf5c0beb067c819
Shuichi Tanaka <shu.at.kantaro.org@gmail.com>
2023-04-17 07:27:36 +0900

    movable dialog

--- resources/js/Pages/Vuetify.vue
+++ resources/js/Pages/Vuetify.vue
@@ -1,41 +1,38 @@
 <script>
-import Layout from "@/Layouts/MainLayout.vue";
+import Layout from "@/Layouts/MainLayout.vue"
 
 export default {
     layout: Layout,
-};
+}
 </script>
 
 <script setup>
-import { ref, watch } from "vue";
-import { Head } from "@inertiajs/vue3";
-import { GoogleMap, Marker } from "vue3-google-map";
+import { ref, watch } from "vue"
+import { Head } from "@inertiajs/vue3"
+import { GoogleMap, Marker } from "vue3-google-map"
 
-const props = defineProps(["canLogin", "canRegister"]);
+const props = defineProps(["canLogin", "canRegister"])
 
-const apiKey = ref(import.meta.env.VITE_GOOGLE_MAPS_KEY);
-const center = ref({ lat: 40.689247, lng: -74.044502 });
+const apiKey = ref(import.meta.env.VITE_GOOGLE_MAPS_KEY)
+const center = ref({ lat: 40.689247, lng: -74.044502 })
 
 const gmap = ref(null)
 
-watch(()=> gmap.value?.ready, (ready)=>{
-    if(!ready) {
+watch(() => gmap.value?.ready, (ready) => {
+    if (!ready) {
         console.log('not ready')
         return
     }
     console.log('gmap ready')
 })
 
+    
+
 </script>
 
 <template>
-    <GoogleMap
-        ref="gmap"
-        :api-key="apiKey"
-        style="width: 100%; height: 100%"
-        :center="center"
-        :zoom="15"
-    >
+    <GoogleMap ref="gmap" :api-key="apiKey" style="width: 100%; height: 100%" :center="center" :zoom="15">
         <Marker :options="{ position: center }" />
     </GoogleMap>
 </template>
+
